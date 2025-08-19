b0019ca45181d86883c3c2d2b7907a69c71e68be
Liza Benguerchon <ielyzaveta.rabkina@outlook.com>
2025-08-19 14:16:01 +0300

    Update guessing_game.py

--- final_project/guessing_game.py
+++ final_project/guessing_game.py
@@ -1,6 +1,6 @@
 import random
 def number_guessing_game():
-    random_number = random.randint(1,101)
+    random_number = random.randint(1,100)
     max_attempts = 7
     win = False
     for attempt in range(1, max_attempts+1):
